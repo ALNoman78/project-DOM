48bce4804b8347d24316ea40788584d45c3c682c
ALNoman78 <abdullaalno078@gmail.com>
2024-09-18 18:57:39 +0600

    login form prevent handler added

---
+++ js/login.js
@@ -0,0 +1,10 @@
+// step - 01
+//  login button
+
+document.getElementById('login-btn').addEventListener('click', function(event){
+    event.preventDefault()
+
+    // step - 02
+    const phoneNumber = document.getElementById('phone-number').value;
+    console.log(phoneNumber);
+})
